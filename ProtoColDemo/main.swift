36666a207316a2e37674cfbcd3f3c5f2b30ba0d7
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-21 16:58:45 +0530

    First commit

--- ProtoColDemo/main.swift
+++ ProtoColDemo/main.swift
@@ -5,7 +5,72 @@
 //  Created by Sahid Reza on 21/12/22.
 //
 
-import Foundation
+protocol CanFly{
+    
+    func fly()
+}
 
-print("Hello, World!")
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg(){
+        
+        if isFemale{
+            
+            print("The bird makes a new bird in a shell")
+        }
+        
+    }
+    
+    
+    
+}
+
+class Eagle:Bird,CanFly {
+    
+    func fly() {
+        
+        print("The Egale flaps its wings and lift off into Skky")
+    }
+    
+    
+    func soar(){
+        
+        print("The Egale glides in the air using air currents.")
+    }
+    
+    
+}
+
+struct FlyingMusim {
+    
+    func flyingDemo(flyingObject: CanFly){
+        flyingObject.fly()
+    }
+    
+    
+}
+
+class Penguin:Bird{
+    
+    func swim(){
+        print("The penguin paddels through wate")
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("The aeroplane uses its engine to lift off in to the air")
+    }
+    
+    
+}
+
+let myEagle = Eagle()
+let myPenguin = Penguin()
+let airplane = Airplane()
+
+let flyMusim = FlyingMusim()
+flyMusim.flyingDemo(flyingObject: airplane)
 
